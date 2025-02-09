e152b709bfd533e65d230723e796639f1e0e3deb
𝖒𝖚𝖘𝖙𝖆𝖋𝖆 <121257754+mustafaakalin@users.noreply.github.com>
2025-02-09 11:58:48 +0300

    Initialize C# project with basic arithmetic operations and .NET 9.0 configuration

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+
+public class Program
+{
+    public static double Toplama(double a, double b)
+    {
+        return a + b;
+        // Console.WriteLine("Toplama işlemi sonucu : " + (a + b));
+    }
+
+    public static double Cikarma(double a, double b)
+    {
+        return a - b;
+    }
+
+    public static double Carpma(double a, double b)
+    {
+        return a * b;
+    }
+
+
+    public static double Bolme(double a, double b)
+    {
+        return a / b;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("işlem yapmak için birinci sayıyı giriniz : ");
+        double sayi1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("işlem yapmak için ikinci sayıyı giriniz : ");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("ISTENEN ISLEMIN PARAMETRE DEGERLERI : " + "sayi 1: " + sayi1 + ", " + "sayi 2 : " + sayi2);
+
+        double sonucToplama = Toplama(sayi1, sayi2);
+        Console.WriteLine("Toplama işlemi sonucu : " + sonucToplama);
+        double sonucCikarma = Cikarma(sayi1, sayi2);
+        Console.WriteLine("Çıkarma işlemi sonucu : " + sonucCikarma);
+        double sonucCarpma = Carpma(sayi1, sayi2);
+        Console.WriteLine("Çarpma işlemi sonucu : " + sonucCarpma);
+        double sonucBolme = Bolme(sayi1, sayi2);
+        Console.WriteLine("Bölme işlemi sonucu : " + sonucBolme);
+
+
+    }
+}
